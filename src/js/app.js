3fbebee0f8a6929b2038304b411925f25bafba35
Jorge Anthony Sanchez Chavez <jorgesanchez99dev@gmail.com>
2025-01-15 16:18:17 -0500

    mejoras en el performance web: uso de imagenes avif

--- src/js/app.js
+++ src/js/app.js
@@ -24,9 +24,12 @@ function crearGaleria(){
     const galeria = document.querySelector('.galeria-imagenes');
 
     for(let i=1;i<=CANTIDAD_IMAGENES ; i++){
-        const imagen = document.createElement('IMG');
-        imagen.src=`src/img/gallery/full/${i}.jpg`;
-        imagen.alt='Imagen galeria';
+        const imagen = document.createElement('PICTURE');
+        imagen.innerHTML = `
+    <source srcset="build/img/gallery/thumb/${i}.avif" type="image/avif">
+    <source srcset="build/img/gallery/thumb/${i}.webp" type="image/webp">
+    <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
+    `;
 
         
         //*Event Handler : Es una funcion que se ejecuta cuando un evento ocurre, en este caso el evento es el click
@@ -39,9 +42,12 @@ function crearGaleria(){
 }
 
 function mostrarImagen(i){
-    const imagen = document.createElement('IMG');
-    imagen.src=`src/img/gallery/full/${i}.jpg`;
-    imagen.alt='Imagen galeria';
+    const imagen = document.createElement('PICTURE');
+    imagen.innerHTML = `
+    <source srcset="build/img/gallery/full/${i}.avif" type="image/avif">
+    <source srcset="build/img/gallery/full/${i}.webp" type="image/webp">
+    <img loading="lazy" width="200" height="300" src="build/img/gallery/full/${i}.jpg" alt="imagen galeria">
+    `;
     
     //*Generar modal
     const modal = document.createElement('DIV');
